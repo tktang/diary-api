1b647eb8617b01017f1d8f067b06042ec0c1d0c7
oluchi <oluchi-orji>
2020-07-17 22:43:15 +0100

    add custom error pages

--- api/app.py
+++ api/app.py
@@ -3,10 +3,16 @@
 from flasgger import Swagger
 from flask import Flask
 from flask_restful import Api
+from webargs.flaskparser import abort, parser
 
 from api.config import env_config
+from utils import errors
+from werkzeug import exceptions
+
 
 api = Api()
+
+
 
 
 def create_app(config_name):
@@ -16,9 +22,46 @@ def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(env_config[config_name])
-    api.init_app(app)
     
-
+   
+    
+    #register api 
+    api.init_app(app)
     Swagger(app)
     
+   
+    #error handling
+    app.register_error_handler(exceptions.NotFound,
+                               errors.handle_404_errors)
+
+    app.register_error_handler(exceptions.InternalServerError,
+                               errors.handle_server_errors)
+
+    app.register_error_handler(exceptions.BadRequest,
+                               errors.handle_400_errors)
+
+    app.register_error_handler(FileNotFoundError,
+                               errors.handle_400_errors)
+
+    app.register_error_handler(TypeError, errors.handle_400_errors)
+
+    app.register_error_handler(KeyError, errors.handle_404_errors)
+
+    app.register_error_handler(AttributeError,
+                               errors.handle_400_errors)
+
+    app.register_error_handler(ValueError, errors.handle_400_errors)
+
+    app.register_error_handler(AssertionError,
+                               errors.handle_400_errors)
+    
+    
+    #new code
+    @parser.error_handler
+    def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
+        """webargs error handler that uses Flask-RESTful's abort function to return
+        a JSON error response to the client.
+        """
+        abort(error_status_code, errors=err.messages)
+        
     return app
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 
+from dotenv import load_dotenv
 
 from api.app import create_app
 
+
+load_dotenv(verbose=True)
+
 app = create_app(os.getenv("FLASK_ENV"))
+
+
+
+
+
+
 
