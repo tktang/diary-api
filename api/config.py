d8cd36f2f7bd40958ef741ff19089119d70f087d
oluchi <oluchi-orji>
2020-08-28 19:17:50 +0100

    add environment variables

--- api/config.py
+++ api/config.py
@@ -45,6 +45,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
+    SECRET_KEY="GGggjjjfk887856$%kk"
     # Disable CSRF protection in the testing configuration
     WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "test.sqlite")
